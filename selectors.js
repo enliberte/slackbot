31631eb6f014bff0d0b5cd2a3a3c03aa8b9b6a5a
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-02 16:41:11 +0300

    'mongo_test'

--- selectors.js
+++
@@ -1,7 +0,0 @@
-const getFollowersSelector = (followed, repoName) => ({followed, repoName});
-
-const getSubscribeSelector = (followed, follower, repoName) => ({followed, follower, repoName});
-
-
-
-module.exports = {getFollowersSelector, getSubscribeSelector};
