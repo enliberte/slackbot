3183018e62670e9d106e08e4df01c80ac09dca8c
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-02 14:07:33 +0300

    'mongo_test'

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 const getSubscribeSelector = require('./selectors').getSubscribeSelector;
 
 const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true });
-const getSubscribes = () => client.db("subscribes").collection("followed");
+const getSubscribes = (client) => client.db("subscribes").collection("followed");
 
 const subscribe = (followed, follower, repoName) => {
     client.connect(err => {
-        const subscribes = getSubscribes();
+        const subscribes = getSubscribes(client);
         const subscribe = getSubscribeSelector(followed, follower, repoName);
         subscribes.update(subscribe, subscribe, {upsert: true});
         client.close()
@@ -17,7 +17,7 @@ const subscribe = (followed, follower, repoName) => {
 
 const unsubscribe = (followed, follower, repoName) => {
     client.connect(err => {
-        const subscribes = getSubscribes();
+        const subscribes = getSubscribes(client);
         subscribes.deleteOne(getSubscribeSelector(followed, follower, repoName));
         client.close()
     })
