9cf9d21e7a19609143aad97438b5e462f596d05b
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-02 14:15:21 +0300

    'mongo_test'

--- db.js
+++ db.js
@@ -7,18 +7,28 @@ const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true })
 
 const subscribe = (followed, follower, repoName) => {
     client.connect(err => {
-        const subscribes = client.db("subscribes").collection("followed");
-        const subscribe = getSubscribeSelector(followed, follower, repoName);
-        subscribes.update(subscribe, subscribe, {upsert: true});
-        client.close()
+        try {
+            const subscribes = client.db("subscribes").collection("followed");
+            const subscribe = getSubscribeSelector(followed, follower, repoName);
+            subscribes.updateOne(subscribe, subscribe, {upsert: true});
+        } catch (e) {
+            console.log(e);
+        } finally {
+            client.close()
+        }
     })
 };
 
 const unsubscribe = (followed, follower, repoName) => {
     client.connect(err => {
-        const subscribes = client.db("subscribes").collection("followed");
-        subscribes.deleteOne(getSubscribeSelector(followed, follower, repoName));
-        client.close()
+        try {
+            const subscribes = client.db("subscribes").collection("followed");
+            subscribes.deleteOne(getSubscribeSelector(followed, follower, repoName));
+        } catch (e) {
+            console.log(e);
+        } finally {
+            client.close()
+        }
     })
 };
 
