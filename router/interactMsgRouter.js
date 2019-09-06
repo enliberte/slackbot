d33f8640b28a896a27b9e57e4bf8d5559b236395
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-06 16:56:59 +0300

    'Refactoring'

--- router/interactMsgRouter.js
+++ router/interactMsgRouter.js
@@ -8,7 +8,6 @@ const {SIGNING_SECRET} = require('./../config');
 const slackInteractions = createMessageAdapter(SIGNING_SECRET);
 
 const processMessages = (payload, respond) => {
-    let followed, reponame, follower;
     const value = payload.actions[0].value;
     const args = value.split('_');
     switch (args[0]) {
@@ -22,22 +21,20 @@ const processMessages = (payload, respond) => {
             new UserAPI(payload.channel.id, null, respond).list(args[1]);
             break;
         case 'follow':
-            [, followed, reponame] = args;
-            follower = payload.user.username;
-            new SubscribeAPI(payload.channel.id, respond).subscribe({followed, follower, reponame});
+            new SubscribeAPI(payload.channel.id, respond).subscribe({
+                followed: args[1], follower: payload.user.username, reponame: args[2]
+            });
             break;
         case 'unfollow':
-            [, followed, reponame] = args;
-            follower = payload.user.username;
-            new SubscribeAPI(payload.channel.id, respond).unsubscribe({followed, follower, reponame});
+            new SubscribeAPI(payload.channel.id, respond).unsubscribe({
+                followed: args[1], follower: payload.user.username, reponame: args[2]
+            });
             break;
         case 'deleteRepo':
-            [, reponame] = args;
-            new RepoAPI(payload.channel.id, null, respond).delete({reponame});
+            new RepoAPI(payload.channel.id, null, respond).delete({reponame: args[1]});
             break;
         case 'deleteUser':
-            [, username] = args;
-            new UserAPI(payload.channel.id, null, respond).delete({username});
+            new UserAPI(payload.channel.id, null, respond).delete({username: args[1]});
             break;
     }
 };
