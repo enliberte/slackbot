4e17cbee7a54970565af56187be84edea36402b3
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-06 16:23:45 +0300

    'Refactoring'

--- api/BaseAPI.js
+++ api/BaseAPI.js
@@ -7,16 +7,12 @@ class BaseAPI {
         this.channelId = channelId;
         this.res = res;
         this.respond = respond;
-    }
-
-    async post(msg) {
-        const postFunc = this.res ?
+        this.post = this.res ?
             async msg => {
                 await this.web.chat.postMessage({...msg, channel: this.channelId});
                 await this.res.status(200).send();
             } :
             async msg => {await this.respond(msg)};
-        await postFunc(msg);
     }
 }
 
