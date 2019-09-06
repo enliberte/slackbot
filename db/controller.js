a9ae2a34ddbb796c757556c72e7fe3a481ffaf03
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-06 14:24:19 +0300

    'Refactoring'

--- db/controller.js
+++ db/controller.js
@@ -3,7 +3,7 @@ const {MONGO_URI} = require('./../config');
 
 class DBController {
     constructor(controller) {
-        this.controller = controller;
+        this.controller = new controller;
         mongoose.connect(MONGO_URI, {useNewUrlParser: true, keepAlive: true});
     }
 
