baa469deadfa74465fed279977b2cae17fbc5623
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-09 15:44:35 +0300

    'replace'

--- router/interactMsgRouter.js
+++ router/interactMsgRouter.js
@@ -7,6 +7,8 @@ const InteractiveMessagesRouter = express.Router();
 const {SIGNING_SECRET} = require('./../config');
 const slackInteractions = createMessageAdapter(SIGNING_SECRET);
 
+const getMsg = async (promise) => ({...await promise, replace_original: true});
+
 const processMessages = async (payload, respond) => {
     const value = payload.actions[0].value;
     const args = value.split('_');
@@ -15,33 +17,30 @@ const processMessages = async (payload, respond) => {
             respond({text: 'See you later'});
             break;
         case 'return':
-            respond({...await new RepoAPI(payload.channel.id).list(), replace_original: true});
+            respond(await getMsg(new RepoAPI(payload.channel.id).list()));
             break;
         case 'select':
-            respond({...await new UserAPI(payload.channel.id).list(args[1]), replace_original: true});
+            respond(await getMsg(new UserAPI(payload.channel.id).list(args[1])));
             break;
         case 'follow':
             await new SubscribeAPI(payload.channel.id).subscribe({
                 followed: args[1], follower: payload.user.username, reponame: args[2]
             });
-            respond({...await new UserAPI(payload.channel.id).list(args[2]), replace_original: true});
+            respond(await getMsg(new UserAPI(payload.channel.id).list(args[2])));
             break;
         case 'unfollow':
             await new SubscribeAPI(payload.channel.id).unsubscribe({
                 followed: args[1], follower: payload.user.username, reponame: args[2]
             });
-            respond({...await new UserAPI(payload.channel.id).list(args[2]), replace_original: true});
+            respond(await getMsg(new UserAPI(payload.channel.id).list(args[2])));
             break;
         case 'deleteRepo':
             await RepoAPI(payload.channel.id).delete({reponame: args[1]});
-            respond({
-                ...await new RepoAPI(payload.channel.id).list('Delete', 'deleteRepo'),
-                replace_original: true
-            });
+            respond(await getMsg(new RepoAPI(payload.channel.id).list('Delete', 'deleteRepo')));
             break;
         case 'deleteUser':
             await new UserAPI(payload.channel.id, null, respond).delete({username: args[1]});
-            respond({...await new UserAPI(payload.channel.id).list(), replace_original: true});
+            respond(await getMsg(new UserAPI(payload.channel.id).list()));
             break;
     }
 };
