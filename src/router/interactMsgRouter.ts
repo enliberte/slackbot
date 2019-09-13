05dd83038d8df4e7a5036af915d421ab41199261
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-13 11:49:08 +0300

    'bugfix'

--- src/router/interactMsgRouter.ts
+++ src/router/interactMsgRouter.ts
@@ -21,6 +21,10 @@ const processMessages = async (payload: any, respond: any) => {
             respond(await getMsg(new RepoAPI(payload.channel.id).list()));
             break;
         case 'select':
+            const msg = await new UserAPI(payload.channel.id).list(args[1]);
+            console.log('---------------------------------------------------');
+            console.log(msg);
+            console.log('---------------------------------------------------');
             respond(await new UserAPI(payload.channel.id).list(args[1]));
             break;
         case 'follow':
