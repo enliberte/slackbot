2bc34bc4d9e5e906d52d171b9fe541fc452b23d3
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-24 15:51:12 +0300

    auth

--- src/db/storageServices/CredentialStorageService.ts
+++ src/db/storageServices/CredentialStorageService.ts
@@ -14,6 +14,6 @@ export default class CredentialStorageService extends BaseStorageService<ICreden
 
     async get(filter: Partial<ICredential>): Promise<ICredential[]> {
         const docs = await this.model.find(filter).exec();
-        return docs.map(doc => ({channelId: doc.channelId, password: doc.password}));
+        return docs.map(doc => ({channelId: doc.channelId, token: doc.token}));
     }
 }
