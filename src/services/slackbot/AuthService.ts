9c1a923a9997a652179f415eb94249e5d25d025d
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-25 09:01:02 +0300

    auth

--- src/services/slackbot/AuthService.ts
+++ src/services/slackbot/AuthService.ts
@@ -8,6 +8,9 @@ export interface IAuthService {
 
 export default class AuthService implements IAuthService {
     createJWT(payload: {channelId: string}): string {
+        console.log('----------------------------------------------');
+        console.log(JWT_SECRET);
+        console.log('----------------------------------------------');
         return sign(payload, JWT_SECRET, {expiresIn: '15m'});
     }
 }
