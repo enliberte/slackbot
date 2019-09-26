fe684df39035a1a222e3e2d36185461bd3788781
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-26 13:49:26 +0300

    auth

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -12,7 +12,8 @@ export default class MainController extends BaseController {
     makeRouter(): Router {
         // this.router.get('/', userAuth, this.showMain.bind(this));
         this.router.get('/', (req, res) => res.status(200).send('HI'));
-        this.router.get('/slack', passport.authorize('Slack'));
+        this.router.get('/slack', passport.authenticate('Slack', { failureRedirect: '/login' }),
+            (req, res) => res.redirect('/'));
         return this.router;
     }
 }
