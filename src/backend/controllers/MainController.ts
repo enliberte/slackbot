5b3d95cd2169b220fed055dc27836fee520a4d99
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-27 16:41:30 +0300

    admin

--- src/backend/controllers/MainController.ts
+++ src/backend/controllers/MainController.ts
@@ -7,12 +7,11 @@ const {DIST} = require('../../../config');
 
 export default class MainController extends BaseController {
     showMain(req: Request, res: Response): void {
-        express.static(DIST);
         res.sendFile(path.resolve(DIST, 'index.html'));
     }
 
     makeRouter(): Router {
-        this.router.get('/', userAuth, this.showMain.bind(this));
+        this.router.get('/', userAuth, express.static(DIST), this.showMain.bind(this));
         return this.router;
     }
 }
