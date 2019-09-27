0b24478a872f757e1b2653e2fc6d3034986111fa
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-09-27 14:18:43 +0300

    admin

--- src/backend/controllers/MainController.ts
+++ src/backend/controllers/MainController.ts
@@ -1,16 +1,12 @@
-import {Request, Response, Router} from "express";
+import express, {Request, Response, Router} from "express";
 const path = require('path');
 import BaseController from "./BaseController";
 import {userAuth} from "../middlewares/auth";
 
 
 export default class MainController extends BaseController {
-    showMain(req: Request, res: Response): void {
-        res.sendFile(path.resolve(__dirname + '/dist', 'index.html'));
-    }
-
     makeRouter(): Router {
-        this.router.get('/', userAuth, this.showMain.bind(this));
+        this.router.get('/', userAuth, express.static(__dirname + '/dist'));
         return this.router;
     }
 }
