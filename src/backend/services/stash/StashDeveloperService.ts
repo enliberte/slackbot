400374ec83c575dcd117c110d73abbe5e82bdbf2
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-10-01 11:45:48 +0300

    admin

--- src/backend/services/stash/StashDeveloperService.ts
+++ src/backend/services/stash/StashDeveloperService.ts
@@ -19,13 +19,11 @@ export default class StashDeveloperService implements IStashDeveloperService {
 
     async list(query: IGetStashDevelopersQuery): Promise<IStashDeveloper[] | false> {
         const url = `/users?${queryString.stringify(query)}`;
-        console.log('-----------------------------------------');
-        console.log(url);
-        console.log('-----------------------------------------');
         try {
             const response = await StashClient.get(url);
             return response.data;
         } catch (e) {
+            console.log(JSON.stringify(e));
             return false;
         }
     }
