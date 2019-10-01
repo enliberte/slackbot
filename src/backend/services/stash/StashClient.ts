bc606bd09d9bf058e7309522e4d898e211e66ab2
Alexey Sumatokhin.EXT <alexeysu@firmglobal.com>
2019-10-01 11:54:08 +0300

    admin

--- src/backend/services/stash/StashClient.ts
+++ src/backend/services/stash/StashClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 const {BITBUCKET_CREDENTIALS} = require('../../../../config');
 
 const StashClient = axios.create({
-    baseURL: 'https://stash.firmglobal.com/rest/api/1.0/',
+    baseURL: 'stash.firmglobal.com/rest/api/1.0/',
     withCredentials: true,
     headers: {
         Authorization: `Basic ${BITBUCKET_CREDENTIALS}`
